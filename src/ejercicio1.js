ae993950c285a3cf8e324c2644af8b8ef1452506
gustavo yevara <gg1904105@gmail.com>
2025-04-10 17:05:00 -0300

    GustavoYevara

---
+++ src/ejercicio1.js
@@ -0,0 +1,4 @@
+var numA =8;
+var numB =10;
+var suma = numA+numB;
+console.log("resulyado de la suma"+suma);
--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-console.log("hello, world")
-console.log("this is javascript")
+console.log("hello, world");
+console.log("this is javascript");
