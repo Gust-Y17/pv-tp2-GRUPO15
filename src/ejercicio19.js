cdfef485bb40039ffb16cea1e47ac33d6558d3a7
gustavo yevara <gg1904105@gmail.com>
2025-04-18 18:14:47 -0300

    ejercicios 18y19 resueltos por Gustavo Yevara

---
+++ src/ejercicio19.js
@@ -0,0 +1,7 @@
+document.querySelector("#mostrarDatos").addEventListener("click", () => {
+    const nombre = document.querySelector("#nombre").value;
+    const apellido = document.querySelector("#apellido").value;
+    const libreta = document.querySelector("#libreta").value;
+  
+    alert(`Los datos ingresados son:\nNombre: ${nombre}\nApellido: ${apellido}\nLibreta Universitaria: ${libreta}`);
+  });
