b73b46e18149c72bc52e7b72ff66c8a00db7492f
JuanPoNZEtti <juanponzetti5@gmail.com>
2025-04-20 21:59:41 -0300

    Reactulizacion de nuevo

--- assets/pages/src/punto21.js
+++
@@ -1,7 +0,0 @@
-const entrada = document.getElementById("entrada");
-const salida = document.getElementById("resultado");
-
-entrada.addEventListener("input", function(){
-salida.textContent = entrada.value;
-}
-);
